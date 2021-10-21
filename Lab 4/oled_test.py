33aaeabef3de53f82450868d4da8de07768c64b7
kristjanari <kt476@cornell.edu>
2021-10-21 14:19:11 -0400

    start

--- Lab 4/oled_test.py
+++ Lab 4/oled_test.py
@@ -2,8 +2,13 @@
 # SPDX-License-Identifier: MIT
 
 import board
+import random
 import busio
+import time
 import adafruit_ssd1306
+import adafruit_mpr121
+
+
 
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -13,36 +18,9 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # to the right size for your display!
 oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
 
+#Create capatitive sensor
+mpr121 = adafruit_mpr121.MPR121(i2c)
 
-# Helper function to draw a circle from a given position with a given radius
-# This is an implementation of the midpoint circle algorithm,
-# see https://en.wikipedia.org/wiki/Midpoint_circle_algorithm#C_example for details
-def draw_circle(xpos0, ypos0, rad, col=1):
-    x = rad - 1
-    y = 0
-    dx = 1
-    dy = 1
-    err = dx - (rad << 1)
-    while x >= y:
-        oled.pixel(xpos0 + x, ypos0 + y, col)
-        oled.pixel(xpos0 + y, ypos0 + x, col)
-        oled.pixel(xpos0 - y, ypos0 + x, col)
-        oled.pixel(xpos0 - x, ypos0 + y, col)
-        oled.pixel(xpos0 - x, ypos0 - y, col)
-        oled.pixel(xpos0 - y, ypos0 - x, col)
-        oled.pixel(xpos0 + y, ypos0 - x, col)
-        oled.pixel(xpos0 + x, ypos0 - y, col)
-        if err <= 0:
-            y += 1
-            err += dy
-            dy += 2
-        if err > 0:
-            x -= 1
-            dx += 2
-            err += dx - (rad << 1)
-
-
-# initial center of the circle
 center_x = 63
 center_y = 15
 # how fast does it move in each direction
@@ -51,37 +29,30 @@ y_inc = 1
 # what is the starting radius of the circle
 radius = 8
 
+
 # start with a blank screen
 oled.fill(0)
 # we just blanked the framebuffer. to push the framebuffer onto the display, we call show()
 oled.show()
+
+colors = ["Banana", "Apple", "Lime", "Orange"]
+sensors = [0, 11, 7, 2]
+
+score = 0
+
 while True:
-    # undraw the previous circle
-    draw_circle(center_x, center_y, radius, col=0)
 
-    # if bouncing off right
-    if center_x + radius >= oled.width:
-        # start moving to the left
-        x_inc = -1
-    # if bouncing off left
-    elif center_x - radius < 0:
-        # start moving to the right
-        x_inc = 1
-
-    # if bouncing off top
-    if center_y + radius >= oled.height:
-        # start moving down
-        y_inc = -1
-    # if bouncing off bottom
-    elif center_y - radius < 0:
-        # start moving up
-        y_inc = 1
-
-    # go more in the current direction
-    center_x += x_inc
-    center_y += y_inc
-
-    # draw the new circle
-    draw_circle(center_x, center_y, radius)
-    # show all the changes we just made
+    index = random.randint(0, 3)
+    oled.text(colors[index], 0, 10, 1)
+    oled.text(str(score), 20, 10, 1)
     oled.show()
+
+    print(colors[index])
+    t_end = time.time() + 5
+    while time.time() < t_end:
+        print("well hello")
+        if mpr121[sensors[index]].value:
+            print(f"Twizzler {index} touched!")
+            score += 1
+    time.sleep(random.randint(0,5))  # Small delay to keep from spamming output messages.
+    
